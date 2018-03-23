ad132d6caf8933b5075d4355d32ada59a2e94aff
cereceres <jesus.edelcereceres@gmail.com>
2018-03-23 11:09:29 -0700

    the test are ready and console are removed

--- index.js
+++ index.js
@@ -9,17 +9,12 @@ module.exports = (arrayToListen = [], future = 1, ...extraArgs) => {
     const queue = [];
     let queueing = null;
     const next = (error, ...argsWithNextExec) => {
-        console.log(' argsWithNextExec ', argsWithNextExec);
-        console.log('runnnig ', running);
         running--;
         queueing(null, error, ...argsWithNextExec);
     };
     queueing = (toExec, errorPassed, ...otherArgs) => {
-        console.log('################## queueing #################3');
-        console.log('otherArgs ', otherArgs);
-        console.log('queue ', queue.length);
         if (running >= future && isFunc(toExec)) return queue.push(toExec);
-        console.log('ejecutando ya%%%%%%%%%%%%%%%%%%%%%%');
+
         if (running >= future) return;
 
         if (!isFunc(toExec)) toExec = queue.shift();
@@ -27,10 +22,8 @@ module.exports = (arrayToListen = [], future = 1, ...extraArgs) => {
         if (!isFunc(toExec)) return;
 
         try {
-            console.log('toExec ', toExec);
             process.nextTick(toExec, next, errorPassed, ...extraArgs, ...otherArgs);
             running++;
-            console.log('runnnig =====', running);
         } catch (error) {
             return next(error);
         }
